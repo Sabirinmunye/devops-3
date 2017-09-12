a0e9cba7f57900daa8a8def43401ee55ea67ca30
Sabirin Munye <sabirinmunye@gmail.com>
2017-09-12 15:26:36 +0100

    ready to go"

--- environment/provision.sh
+++ environment/provision.sh
@@ -3,7 +3,13 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
+sudo rm /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/app/environment/nginx.default /etc/nginx/sites-available/default
+sudo service nginx restart
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo npm install -g pm2
+
+
+
